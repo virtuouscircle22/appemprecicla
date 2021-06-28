d20e0e3615eaf44197ebf31f374f4b4200fc0438
ASS <androisaco@gmail.com>
2021-06-28 11:03:49 +0200

    first commit

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
-import { UserService } from "../../services/User.service";
+import { UserService } from "../../services/user.service";
 import { NgForm } from "@angular/forms";
 import { User } from "../../models/User";
 import { Router } from '@angular/router';
